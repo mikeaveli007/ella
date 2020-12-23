dcdcd54c8fa14376add6e59599bec7e399b77b01
Clark Wachsmuth <wachsmuthc@ochin.org>
2020-12-22 18:42:33 -0800

    Update OR syntax for config file

--- config.php
+++ config.php
@@ -4,13 +4,13 @@ unset($CFG);
 global $CFG;
 $CFG = new stdClass();
 
-$CFG->dbtype    = $_ENV['ELLA_DB_TYPE'] || 'mysqli';
-$CFG->dblibrary = $_ENV['ELLA_DB_LIBRARY'] || 'native';
-$CFG->dbhost    = $_ENV['ELLA_DB_HOST'] || '172.25.0.2';
-$CFG->dbname    = $_ENV['ELLA_DB_NAME'] || 'moodle';
-$CFG->dbuser    = $_ENV['ELLA_DB_USER'] || 'user';
-$CFG->dbpass    = $_ENV['ELLA_DB_PASS'] || 'password';
-$CFG->prefix    = $_ENV['ELLA_DB_PREFIX'] || 'mdl_';
+$CFG->dbtype    = $_ENV['ELLA_DB_TYPE'] ?? 'mysqli';
+$CFG->dblibrary = $_ENV['ELLA_DB_LIBRARY'] ?? 'native';
+$CFG->dbhost    = $_ENV['ELLA_DB_HOST'] ?? '172.25.0.2';
+$CFG->dbname    = $_ENV['ELLA_DB_NAME'] ?? 'moodle';
+$CFG->dbuser    = $_ENV['ELLA_DB_USER'] ?? 'user';
+$CFG->dbpass    = $_ENV['ELLA_DB_PASS'] ?? 'password';
+$CFG->prefix    = $_ENV['ELLA_DB_PREFIX'] ?? 'mdl_';
 $CFG->dboptions = array (
   'dbpersist' => 0,
   'dbport' => '',
@@ -18,10 +18,10 @@ $CFG->dboptions = array (
   'dbcollation' => 'utf8mb4_general_ci',
 );
 
-$CFG->wwwroot   = $_ENV['ELLA_WWWROOT'] || 'http://localhost:8000';
-$CFG->dataroot  = $_ENV['ELLA_DATAROOT'] || '/workspace/moodledata';
-$CFG->tempdir = $_ENV['ELLA_TEMPDIR'] || $CFG->tempdir;
-$CFG->cachedir = $_ENV['ELLA_CACHEDIR'] || $CFG->cachedir;
+$CFG->wwwroot   = $_ENV['ELLA_WWWROOT'] ?? 'http://localhost:8000';
+$CFG->dataroot  = $_ENV['ELLA_DATAROOT'] ?? '/workspace/moodledata';
+$CFG->tempdir = $_ENV['ELLA_TEMPDIR'] ?? $CFG->tempdir;
+$CFG->cachedir = $_ENV['ELLA_CACHEDIR'] ?? $CFG->cachedir;
 $CFG->admin     = 'admin';
 
 $CFG->directorypermissions = 0777;
