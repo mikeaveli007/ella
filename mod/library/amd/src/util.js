80c224496d23ee236a62cf4e44ffc1729d5a858d
Michael Ruiz <mikeaveli007@hotmail.com>
2021-10-27 15:40:35 -0700

    convert additional plugins to submodules

--- mod/library/amd/src/util.js
+++
@@ -1,7 +0,0 @@
-define(['jquery', 'mod_library/jquery.ui.pos', 'mod_library/iconpicker'], function($, pos, iconpicker) {
-    return {
-        init: function() {
-            $('#icon').iconpicker();
-        }
-    };
-});
--- report/extendedlog/amd/README.md
+++
@@ -1,3 +0,0 @@
-To build on windows use
- 
-    grunt js --root=report/extendedlog
