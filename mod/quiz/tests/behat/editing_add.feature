moodle/moodle

MDL-74211 quiz: make use of question data generators

mod/quiz/tests/behat/editing_add.feature
@@ -99,8 +99,7 @@ Feature: Edit quiz page - adding things
       in various categories and add them to the question bank.
 
     # Create a couple of sub categories.
-    When I am on "Course 1" course homepage
-    And I navigate to "Question bank" in current page administration
+    When I am on the "Course 1" "core_question > course question bank" page
     And I select "Categories" from the "Question bank tertiary navigation" singleselect
     Then I should see "Add category"
     And I follow "Add category"

mikeaveli007/ella

mod/quiz/tests/behat/editing_add.feature
@@ -105,8 +105,7 @@ Feature: Edit quiz page - adding things
       in various categories and add them to the question bank.
 
     # Create a couple of sub categories.
-    When I am on "Course 1" course homepage
-    And I navigate to "Question bank > Categories" in current page administration
+    When I am on the "Course 1" "core_question > course question categories" page
     Then I should see "Add category"
     Then I set the field "Parent category" to "Default for C1"
     And I set the field "Name" to "Subcat 1"